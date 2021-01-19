a289fee25511e768b9bd45d8c59af100780c2848
TobaMan <72600776+TobaMan@users.noreply.github.com>
2021-01-19 18:54:18 +0100

    Update documentation

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,7 +40,12 @@ function GetVerNewList(vername) {
 var cur_version = 0;
 var allversions = 0;
 var no_download = "The setup program for this platform is not available for this version.";
-//var newlist = [];
+var black_code_col = "rgba(0, 0, 0, 0.25)";
+var white_code_col = "rgba(245, 242, 240, 0.25)";
+var black_ccode_col = "rgba(0, 32, 32, 0.25)";
+var white_ccode_col = "rgba(215, 212, 210, 0.45)";
+var code_col = black_code_col;
+var ccode_col = black_ccode_col;
 
 
 jQuery(function ($) {
@@ -169,7 +174,10 @@ function SetMacDownloadInfo(vername, info) {
 
 function InitDocToc() {
     $(function () {
-        Toc.init($("#toc"));
+        Toc.init({
+            $nav: $("#toc")
+            //$scope: $("h1, h2, h3")
+        });
     });
 }
 
@@ -227,6 +235,67 @@ function ResponsiveDocToc() {
     }
 }
 
+function SetDocCodeClass(color, lang) {
+    $("td").each(function () {
+        var incol = $(this).css("background-color");
+        if (incol === color) {
+            $(this).addClass(lang);
+        }
+    });
+    $("table").each(function () {
+        var incol = $(this).css("background-color");
+        $(this).css("background-color", code_col);
+        if (incol === color) {
+            $(this).find('td').each(function () {
+                $(this).addClass(lang);
+
+            });
+        }
+    });
+}
+
+function FindDocCodeStyle() {
+    var tolang = "toba-code";
+    var coltoba = 'rgb(242, 242, 242)';
+    SetDocCodeClass(coltoba, tolang);
+    var clang = "clang-code";
+    var colclang = 'rgb(238, 236, 225)';
+    SetDocCodeClass(colclang, clang);
+}
+
+function ApplyTobaDocCodeStyle() {
+    $(".toba-code").each(function () {
+        var tobacode = $(this);
+        var code = tobacode.get(0).innerText;
+        var html = Prism.highlight(code, Prism.languages.swift, 'swift');
+        tobacode.html(html);
+        tobacode.css("background", code_col);
+        tobacode.css("background-color", code_col);
+        tobacode.css('text-decoration', 'none');
+        tobacode.css("white-space", "pre");
+        tobacode.css("line-height", "75%");
+        tobacode.css("padding", "20px");
+        //tobacode.css("overflow", "auto");
+        //tobacode.css("font-size", "0.75em");
+        //tobacode.css("font-family", "consolas");
+
+    });
+    $(".clang-code").each(function () {
+        var tobacode = $(this);
+        var code = tobacode.get(0).innerText;
+        var html = Prism.highlight(code, Prism.languages.swift, 'swift');
+        tobacode.html(html);
+        tobacode.css("background", ccode_col);
+        tobacode.css("background-color", ccode_col);
+        tobacode.css('text-decoration', 'none');
+        tobacode.css("white-space", "pre");
+        tobacode.css("line-height", "75%");
+        tobacode.css("padding", "20px");
+    });
+}
+
+
+
 function ApplyDocumentationStyle() {
 
     var adapt_content = false;
@@ -234,17 +303,83 @@ function ApplyDocumentationStyle() {
         TableFitWindow();
     }
 
-    $('table').css("border", "solid var(--primary) 1px");
-    $('table').css("border-left", "solid var(--primary) 1px");
-    $('table').css("border-right", "solid var(--primary) 1px");
-    $('table').css("border-top", "solid var(--primary) 1px");
-    $('table').css("border-bottom", "solid var(--primary) 1px");
-    $('table').css("background", "none");
+    //    $('table').css("border", "solid var(--primary) 1px");
+    //    $('table').css("border-left", "solid var(--primary) 1px");
+    //    $('table').css("border-right", "solid var(--primary) 1px");
+    //    $('table').css("border-top", "solid var(--primary) 1px");
+    //    $('table').css("border-bottom", "solid var(--primary) 1px");
+    //
+    //    $('td').css("border", "solid var(--primary) 1px");
+    //    $('td').css("border-left", "solid var(--primary) 1px");
+    //    $('td').css("border-right", "solid var(--primary) 1px");
+    //    $('td').css("border-top", "solid var(--primary) 1px");
+    //    $('td').css("border-bottom", "solid var(--primary) 1px");
+
+    //    $('table').removeAttr("width");
+    //    $('td').removeAttr("width");
+    //    $('tr').removeAttr("width");
+    //    $('table').removeAttr("valign");
+    //    $('td').removeAttr("valign");
+    //    $('tr').removeAttr("valign");
+    //    $('table').removeAttr("border");
+    //    $('td').removeAttr("border");
+    //    $('tr').removeAttr("border");
+    //    $('table').removeAttr("cellspacing");
+    //    $('td').removeAttr("cellspacing");
+    //    $('tr').removeAttr("cellspacing");
+    //    $('table').removeAttr("cellpadding");
+    //    $('td').removeAttr("cellpadding");
+    //    $('tr').removeAttr("cellpadding");
+
+    //    $('table').css("width", "100vw");
+    //    $('table').css("table-layout", "fixed");
+
+
+    $('p').css("background", "none");
+    $('span').css("background", "none");
+    //
+    //
+    //    $('.NoteStyle').css("border", "solid rgba(var(--primary), 0.5) 1px");
+    //    $('.NoteStyle').css("background", "rgba(var(--primary), 0.3)");
+    //    $('.NoteStyle').css("background-color", "rgba(var(--primary), 0.3)");
+    //$('.NoteStyle').css("background", "var(--info)");
+    $('.NoteStyle').css("border", "solid rgba(23, 162, 184,0.3) 1px");
+    $('.NoteStyle').css("background", "rgba(23, 162, 184,0.1)");
+    //$('.NoteStyle').css("opacity", "0.3");
+    $('.NoteStyle').css("margin-top", "3px");
+    $('.NoteStyle').css("margin-bottom", "3px");
+    $('.NoteStyle').css("padding", "20px");
+    $('.NoteStyle').css("border-radius", "4px");
+
+    $('table').css("border", "solid rgba(0, 0, 0, 0.35) 1px");
+    $('table').css("border-left", "solid rgba(0, 0, 0, 0.35) 1px");
+    $('table').css("border-right", "solid rgba(0, 0, 0, 0.35) 1px");
+    $('table').css("border-top", "solid rgba(0, 0, 0, 0.35) 1px");
+    $('table').css("border-bottom", "solid rgba(0, 0, 0, 0.35) 1px");
+
+    $('td').css("border", "solid rgba(0, 0, 0, 0.35) 1px");
+    $('td').css("border-left", "solid rgba(0, 0, 0, 0.35) 1px");
+    $('td').css("border-right", "solid rgba(0, 0, 0, 0.35) 1px");
+    $('td').css("border-top", "solid rgba(0, 0, 0, 0.35) 1px");
+    $('td').css("border-bottom", "solid rgba(0, 0, 0, 0.35) 1px");
+
+    //
+    //        $('table').css("background-color", "none");
+    //        $('td').css("background-color", "none");
+    //$('td').css("background", "none");
+
+
+
     //$('body').css("font - size ", "0.7 em ");
 
     $('body').css("font-family", "");
     $('p').css("font-family", "");
     $('span').css("font-family", "");
+
+
+    FindDocCodeStyle();
+    ApplyTobaDocCodeStyle();
+
 
 }
 
@@ -263,10 +398,14 @@ function AddCode(name) {
 
 function SetCodeBlackTheme() {
     $('link[info="code-css"]').attr('href', "assets/css/prism_black.css");
+    code_col = black_code_col;
+    ccode_col = black_ccode_col
 }
 
 function SetCodeWhiteTheme() {
     $('link[info="code-css"]').attr('href', "assets/css/prism_light.css");
+    code_col = white_code_col;
+    ccode_col = white_ccode_col
 }
 
 function LoadDemoCode(democode) {
@@ -281,17 +420,24 @@ function LoadDemoCode2() {
 }
 
 
+
+$(window).on('load', function () {
+
+});
+
+
 $(document).ready(function () {
 
-    ApplyDocumentationStyle();
-    ResponsiveDocToc();
 
     var theme = GetCurrentTheme();
     if (theme) {
         set_theme(theme);
     } else {
-        set_theme("assets/css/bootstrap.min.Slate.css");
+        set_theme("assets/css/bootstrap.min.Superhero.css");
     }
+
+    ApplyDocumentationStyle();
+    ResponsiveDocToc();
 
     jQuery.get("assets/data/data", function (datav) {
         data = JSON.parse(datav);
