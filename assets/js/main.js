12aa2b398411340803146da8e0d170f302b25b8e
TobaMan <72600776+TobaMan@users.noreply.github.com>
2021-01-21 11:30:47 +0100

    Update User Stats

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
+function UsersStats_goatcounter() {
+
+    $('#id-Contact').att("data-goatcounter-click", "id-Contact");
+    $('#id-Pull-requests').att("data-goatcounter-click", "id-Pull-requests");
+    $('#id-Issues').att("data-goatcounter-click", "id-Issues");
+    $('#id-Themes').att("data-goatcounter-click", "id-Themes");
+    $('#id-documentation').att("data-goatcounter-click", "id-documentation");
+    $('#id-moreinfo').att("data-goatcounter-click", "id-moreinfo");
+    $('#id-started').att("data-goatcounter-click", "id-started");
+    $('#id-download').att("data-goatcounter-click", "id-download");
+    $('#id-install').att("data-goatcounter-click", "id-install");
+    $('#id-create').att("data-goatcounter-click", "id-create");
+    $('#id-run').att("data-goatcounter-click", "id-run");
+    $('#id-donate').att("data-goatcounter-click", "id-donate");
+    $('#id-future').att("data-goatcounter-click", "id-future");
+    $('#id-version').att("data-goatcounter-click", "id-version");
+}
+
 function set_theme(theme) {
     $('link[title="main"]').attr('href', theme);
     ChangeCodeTheme(theme);
